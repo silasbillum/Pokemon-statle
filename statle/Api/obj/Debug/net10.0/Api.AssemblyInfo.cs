926d0490d9f06e26dbe941fce956260087d45fd8
silasbillum <stilhofsvej46@gmail.com>
2026-04-29 15:26:00 +0200

    dokerfiles

--- statle/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ statle/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4481b0fd5fca89d2408c4b20a6835d1f6b293f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6ace827f71517b22845c9a27da6a5220eb2832")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
