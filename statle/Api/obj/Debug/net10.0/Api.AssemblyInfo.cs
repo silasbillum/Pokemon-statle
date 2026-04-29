f86b95dde77b95739f4041c2e2ba8f950a357636
silasbillum <stilhofsvej46@gmail.com>
2026-04-29 08:58:53 +0200

    dd updat

--- statle/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ statle/Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1550317a6c60b664b170a229e1e9e0824994e3ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ed499a884199a46b71b18756245e25c8f72800")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
